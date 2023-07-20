d3f9784d9862fe331b84878bd353853e38efd1b9
JerarDepardie <138195486+JerarDepardie@users.noreply.github.com>
2023-07-20 21:04:06 +0300

    Add files via upload

---
+++ Dz4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// System.Console.Write("Введите число котовое будет возвадится в степень: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число в степень которого хотите возвести предыдущее число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int result = number;
+
+// for (int i = 1; i < number2; i++)
+// {
+//     result *= number;
+// }
+
+// System.Console.WriteLine(result);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (number / 10 != 0)
+// {
+//     sum += number % 10;
+//     number /= 10;
+// }
+// sum += number % 10;
+
+// System.Console.WriteLine(sum);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int[] array = new int[8];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(50);
+//     System.Console.Write(array[i] + " ");
+// }
