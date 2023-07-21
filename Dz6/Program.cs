da7e618fd26e28d254d91e0382aedfa6b225e92d
JerarDepardie <138195486+JerarDepardie@users.noreply.github.com>
2023-07-21 14:52:43 +0300

    Add files via upload

---
+++ Dz6/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// System.Console.WriteLine("Вводите числа через ентер, как только будет достаточно оставте пустую  строку");
+// bool flag = true;
+// int count = 0;
+// string num = "";
+
+// while(flag == true)
+// {
+//     num = Console.ReadLine();
+//     if(num == "")
+//         break;
+//     int number = Convert.ToInt32(num);
+//     if (number > 0)
+//         count++;
+// }
+
+// System.Console.WriteLine($"Больше нуля {count} введеных чисел");
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Console.WriteLine("Уравнение прямой 1 (y = m1 * x + b1):");
+// double m1 = Convert.ToDouble(Console.ReadLine());
+// double b1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Уравнение прямой 2 (y = m2 * x + b2):");
+// double m2 = Convert.ToDouble(Console.ReadLine());
+// double b2 = Convert.ToDouble(Console.ReadLine());
+
+// // Поиск точки пересечения (x, y) методом замены
+// double x = (b2 - b1) / (m1 - m2);
+// double y = m1 * x + b1;
+
+// Console.WriteLine($"Точка пересечения: ({x}, {y})");
+
